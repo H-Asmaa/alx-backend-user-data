969a43c667fc779c51d553fadd4576231d06a089
H-Asmaa <asmaehadar32@gmail.com>
2024-02-11 19:16:59 +0100

    Committing a function that checks the validity of a hashed password.

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,13 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """A function that hashes a password that is passed as string."""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """A function that validates if the encrypt is associated
+    to a password."""
+    return (
+        True
+        if bcrypt.checkpw(password.encode(), hashed_password)
+        else False
+    )
