2f56b0e683be2cf70e12edc8e5d129c8ee750b93
H-Asmaa <asmaehadar32@gmail.com>
2024-02-11 16:37:49 +0100

    Committing the method format that filters incoming log records.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -26,6 +26,7 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List):
+        """The method init of the class."""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
