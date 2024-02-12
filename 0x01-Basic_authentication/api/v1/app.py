68b62408d9a59996ec925abdef20a52c377b9107
H-Asmaa <asmaehadar32@gmail.com>
2024-02-12 18:57:22 +0100

    Committing an update in the requestFilter function, output fail correction.

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -26,11 +26,14 @@ def requestFilter() -> None:
     """A function that filters a request for authorisation"""
     if auth is None:
         return
-    if request.path not in [
-        "/api/v1/status/",
-        "/api/v1/unauthorized/",
-        "/api/v1/forbidden/",
-    ]:
+    if not auth.require_auth(
+        request.path,
+        [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
+        ],
+    ):
         return
     if auth.authorization_header(request) is None:
         abort(401)
