8c89a3b480edb1fad1f507921d43ffcf10f4e7ec
H-Asmaa <asmaehadar32@gmail.com>
2024-02-12 20:11:01 +0100

    Committing the method that returns the Base64 part of the Authorization header for a Basic Authentication. Pycodestyle fail.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,12 +8,13 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """A class that inherits from Auth."""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """A method that returns the Base64 of the Authorization header."""
         return (
-			authorization_header[6:]
-			if authorization_header is not None
-			and isinstance(authorization_header, str)
-			and authorization_header.startswith("Basic ")
-			else None
-		)
+            authorization_header[6:]
+            if authorization_header is not None
+            and isinstance(authorization_header, str)
+            and authorization_header.startswith("Basic ")
+            else None
+        )
