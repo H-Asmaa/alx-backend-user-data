5b868e99b897bafb536ad8534f4ac36f7d27451a
H-Asmaa <asmaehadar32@gmail.com>
2024-02-12 23:33:37 +0100

    Committing an update to require_auth to enable * in the excluded_paths.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,6 +13,9 @@ class Auth:
         """A method that checks if authentication is required for a path."""
         if path and not path.endswith(("/")):
             path = path + "/"
+        for i in range(len(excluded_paths)):
+            if '*' in excluded_paths[i]:
+                excluded_paths[i] = excluded_paths[i][:-1]
         return not path or not excluded_paths or path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
