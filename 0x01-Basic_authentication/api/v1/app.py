cc3ce87a079185022e1e47577d9705fb15a33d25
H-Asmaa <asmaehadar32@gmail.com>
2024-02-12 19:48:09 +0100

    Committing the new class BasicAuth and the app.py updated to create an instance of BasicAuth if the env variable is equal to basic_auth.

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,9 +15,13 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = os.environ.get("AUTH_TYPE")
 
-if auth_type:
-    from api.v1.auth.auth import Auth
 
+
+if auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+0x01. Basic authentication
+"""
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """A class that inherits from Auth."""
+
+    pass
