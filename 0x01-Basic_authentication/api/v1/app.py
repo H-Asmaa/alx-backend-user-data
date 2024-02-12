99a10c78b5ae75867f96025ecd95f9ebfc416fba
H-Asmaa <asmaehadar32@gmail.com>
2024-02-12 18:32:15 +0100

    Committing the new function before request that filters a request.

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,30 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = os.environ.get("AUTH_TYPE")
+
+if auth_type:
+    from api.v1.auth.auth import Auth
+
+    auth = Auth()
+
+
+@app.before_request
+def requestFilter() -> None:
+    """A function that filters a request for authorisation"""
+    if auth is None:
+        return
+    if request.path not in [
+        "/api/v1/status/",
+        "/api/v1/unauthorized/",
+        "/api/v1/forbidden/",
+    ]:
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(404)
