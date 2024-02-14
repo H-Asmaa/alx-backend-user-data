65285590bb56d2c3641f5b1830e96ca518b6534d
H-Asmaa <asmaehadar32@gmail.com>
2024-02-14 23:46:52 +0100

    Committing the class attribute for SessionAuth and the method create_session that creates a session ID for a user_id/

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,18 @@
 0x02-Session_authentication
 """
 from api.v1.auth.basic_auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """A class where to practice session based authentication."""
 
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """A method that creates a session ID for user_id."""
+        if user_id and isinstance(user_id, str):
+            session_id = str(uuid.uuid4())
+            SessionAuth.user_id_by_session_id[session_id] = user_id
+            return session_id
+        return None
