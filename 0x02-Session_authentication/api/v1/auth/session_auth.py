b016897218d01dce8526747ca7c8bd287348d7fa
H-Asmaa <asmaehadar32@gmail.com>
2024-02-14 23:10:00 +0100

    Committing the new class session_auth with the checking of the env variable in the app.py.

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+0x01. Basic authentication
+"""
+from auth import Auth
+
+
+class SessionAuth(Auth):
+    """A class where to practice session based authentication."""
+
+    pass
