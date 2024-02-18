f1123924fe21f523d43b6646cffaf75ea56577f2
H-Asmaa <asmaehadar32@gmail.com>
2024-02-18 14:02:57 +0100

    Committing correction of the method user_id_for_session_id.

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -4,6 +4,7 @@
 """
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
+from datetime import datetime, timedelta
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -13,6 +14,8 @@ class SessionDBAuth(SessionExpAuth):
         """A method that creates and stores new instance
         of UserSession and returns the Session ID"""
         new_session = super().create_session(user_id)
+        if new_session is None:
+            return None
         if isinstance(new_session, str):
             user_session = UserSession(user_id=user_id, session_id=new_session)
             user_session.save()
@@ -23,16 +26,24 @@ class SessionDBAuth(SessionExpAuth):
         """A method that returns the User ID by requesting
         UserSession in the database based on session_id"""
         if session_id and isinstance(session_id, str):
-            userSession = UserSession().search({"session_id": session_id})
-            return userSession[0].user_id if len(userSession) > 0 else None
+            if super().user_id_for_session_id(session_id) is None:
+                return None
+            try:
+                userSession = UserSession().search({"session_id": session_id})
+            except Exception:
+                return None
+            if userSession is None or userSession == []:
+                return None
+            return userSession[0].user_id
         return None
 
     def destroy_session(self, request=None):
         """A method that destroys the UserSession based on the
         Session ID from the request cookie"""
         session_id = self.session_cookie(request)
-        userSession = UserSession().search({"session_id": session_id})
-        if len(userSession):
-            userSession[0].delete()
-            return True
+        if session_id:
+            userSession = UserSession().search({"session_id": session_id})
+            if len(userSession):
+                userSession[0].remove()
+                return True
         return False
