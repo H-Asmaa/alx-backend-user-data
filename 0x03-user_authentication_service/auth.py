66da6456d9a166061cb29c993c559ccb8e85c284
H-Asmaa <asmaehadar32@gmail.com>
2024-02-20 21:08:40 +0100

    Committing the function hash_password.

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+0x03-user_authentication_service
+"""
+import bcrypt
+
+
+def _hash_password(password: str):
+    """A method that returns the hash of a password."""
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
