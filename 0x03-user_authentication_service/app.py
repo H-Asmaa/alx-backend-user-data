9589dcc97ad719f545b46998b5b71fc64b3d5eb5
H-Asmaa <asmaehadar32@gmail.com>
2024-02-21 00:55:10 +0100

    Committing a new route /users
    Returns a message if the user exists and another if it got created.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """
 0x03-user_authentication_service
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -12,6 +14,18 @@ app = Flask(__name__)
 def index():
     """The index method."""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def user():
+    """A function that implements the POST /users route."""
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        AUTH.register_user(email=email, password=password)
+        return jsonify({"email": f"{email}", "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
