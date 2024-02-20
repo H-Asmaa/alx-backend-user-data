9b8a9246c62adc54df3f99c091a76c819fc47e93
H-Asmaa <asmaehadar32@gmail.com>
2024-02-20 15:45:54 +0100

    Committing class DB with a new method that adds a user.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,11 +34,7 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """A method that adds a user to the database."""
-        try:
-            user = User(email=email, hashed_password=hashed_password)
-            self._session.add(user)
-            self.__session.commit()
-            return user
-        except SQLAlchemyError as exception:
-            self.__session.rollback()
-            raise exception
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self.__session.commit()
+        return user
