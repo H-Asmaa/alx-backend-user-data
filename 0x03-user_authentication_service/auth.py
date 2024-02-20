0b30188e87d09b2a688eb55a381671c06a8c972c
H-Asmaa <asmaehadar32@gmail.com>
2024-02-20 23:56:36 +0100

    Committing the class Auth, with the method register_user
    register_user regiters a new user after making sure it doesn't exists and hashing the password.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,8 +3,28 @@
 0x03-user_authentication_service
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """A method that returns the hash of a password."""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """A method that registers a user."""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashedPw = _hash_password(password)
+            user = self._db.add_user(email=email, hashed_password=hashedPw)
+            return user
