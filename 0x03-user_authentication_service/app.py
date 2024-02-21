42baaa24627f83896a37a7ff6fd6384c6eec7455
H-Asmaa <asmaehadar32@gmail.com>
2024-02-21 03:03:50 +0100

    Committing the new route logout.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 0x03-user_authentication_service
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect, url_for
 from auth import Auth
 
 
@@ -41,6 +41,18 @@ def login():
         response = jsonify({"email": f"{email}", "message": "logged in"})
         response.set_cookie("session_id", session_id)
         return response
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    """A function that takes care of the logout."""
+    session_id = request.cookies.get("session_id")
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect(url_for("index"))
+    abort(403)
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/main_10.py
+++ 0x03-user_authentication_service/main_10.py
@@ -12,8 +12,3 @@ auth.register_user(email, password)
 
 session_id = auth.create_session(email)
 print(session_id)
-print(auth.create_session("unknown@email.com"))
-user = auth.get_user_from_session_id(session_id)
-print(user.session_id)
-user = auth.get_user_from_session_id("jssj")
-print(user)
