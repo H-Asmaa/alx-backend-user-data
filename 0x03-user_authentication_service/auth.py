a30bcedc1fd4402924d5fbb3d82e40d033a70b15
H-Asmaa <asmaehadar32@gmail.com>
2024-02-21 01:04:54 +0100

    Committing a new method valid_login that validated a login.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,3 +29,11 @@ class Auth:
             hashedPw = _hash_password(password)
             user = self._db.add_user(email, hashedPw)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """A method that validates a user's login."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
