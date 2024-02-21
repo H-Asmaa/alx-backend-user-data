8a02e070410e476de0e9a1f4d37faaf7677b8a42
H-Asmaa <asmaehadar32@gmail.com>
2024-02-21 03:26:00 +0100

    Committing a new method reset password.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,3 +68,13 @@ class Auth:
             user.session_id = None
         except Exception:
             raise Exception
+
+    def get_reset_password_token(self, email: str) -> str:
+        """A method that restets a password."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        newUuid = _generate_uuid()
+        self._db.update_user(email=email, reset_token=newUuid)
+        return user.reset_token
--- 0x03-user_authentication_service/main_10.py
+++ 0x03-user_authentication_service/main_10.py
@@ -12,3 +12,6 @@ auth.register_user(email, password)
 
 session_id = auth.create_session(email)
 print(session_id)
+user = auth.get_user_from_session_id(session_id)
+print(user.session_id)
+print(auth.get_reset_password_token(email))
