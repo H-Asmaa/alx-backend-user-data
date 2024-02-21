4daae08a69dd04cff00e22d694b1efe5be8e88a2
H-Asmaa <asmaehadar32@gmail.com>
2024-02-21 02:39:30 +0100

    Committing the destroy_session method.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -60,3 +60,11 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except Exception:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """A method that destroys a session."""
+        try:
+            user = self._db.find_user_by(id=user_id)
+            user.session_id = None
+        except Exception:
+            raise Exception
