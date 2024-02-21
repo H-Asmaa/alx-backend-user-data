4fabe60eb95093aa36f10fb7d7ece13077c933ef
H-Asmaa <asmaehadar32@gmail.com>
2024-02-21 03:11:38 +0100

    Committing new function profile.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,5 +55,15 @@ def logout():
     abort(403)
 
 
+@app.route("/profile")
+def profile():
+    """A function that gets user profile."""
+    session_id = request.cookies.get("session_id")
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": f"{user.email}"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
