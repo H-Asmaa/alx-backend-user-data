a53b09065adc158dd3c152e52e3df45cb2aaa0c2
H-Asmaa <asmaehadar32@gmail.com>
2024-02-21 02:28:44 +0100

    Committing a method that gets the user based on the session id.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,10 +56,7 @@ class Auth:
 
     def get_user_from_session_id(self, session_id: str) -> User:
         """A method that gets a user by session id."""
-        if session_id:
-            try:
-                user = self._db.find_user_by(session_id=session_id)
-                return user
-            except NoResultFound:
-                raise NoResultFound
-        return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except Exception:
+            return None
